6c216c66b4b09817589b5739cb26932b4c0e2491
Stephen.Kearney <skearney82@gmail.com>
2015-04-19 01:38:53 +0100

    Added product map routes to seeds file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+growth_hacking = Product.create(title: "Cycle Maps Ireland", 
+ subtitle: "Ireland Best Cycle ROutes", author: "Stephen Kearney", 
+ length: "30 minutes", price: "4.99", sku: "MAP1", 
+ details: "You'll get a map journey for a particular region", description: %{<p>A good cycle route is a rare combination: somewhere with the right views and ease of traversing, within a safe environment and close to interesting stops and esential pit-stops.</p>
+
+ <p>In these Maps, S Kearney introduces you to his favorite cycle routes and tips for getting the most out of them.</p>
+ <p><strong>What You'll Learn</strong></p>
+ <ul class="no-indent">
+ <li>What are Ireland's top cycle routes?</li>
+ <li>The 5 stages of cycling Nirvana in Ireland</li>
+ <li>How to cycle safely in Ireland</li>
+ <li>Resources, people and places you'll need to know along the way</li>
+ </ul>})
